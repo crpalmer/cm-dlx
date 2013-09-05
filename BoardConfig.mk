c834a2d631dcddd887f64cad0006713d221eca8a
Christopher R. Palmer <crpalmer@gmail.com>
2013-09-05 19:55:08 -0400

    Use gcc-4.7 to build
    Change-Id: I5f43b2115ab705b4d6388c46e86876c777695603

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -32,6 +32,7 @@ TARGET_SPECIFIC_HEADER_PATH := device/htc/dlx/include
 # Flags
 TARGET_GLOBAL_CFLAGS += -mfpu=neon-vfpv4 -mfloat-abi=softfp
 TARGET_GLOBAL_CPPFLAGS += -mfpu=neon-vfpv4 -mfloat-abi=softfp
+TARGET_GCC_VERSION_EXP := 4.7
 
 # Assert
 TARGET_OTA_ASSERT_DEVICE := dlx
